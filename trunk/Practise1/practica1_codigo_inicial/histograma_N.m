788315d666efaddae0810164fbb36545da79f66b
oriong2@gmail.com <oriong2@gmail.com@fac77a9e-9cf5-8a52-1d01-95cccab20d58>
2013-10-29 21:58:38 +0000

    Histogram improved. Some changes must be done. var D is misundertood

--- trunk/Practise1/practica1_codigo_inicial/histograma_N.m
+++ trunk/Practise1/practica1_codigo_inicial/histograma_N.m
@@ -1,8 +1,11 @@
-function LABEL = histograma_N( DATOENTRADA,hist,N )
+function LABEL = histograma_N( DATOENTRADA,hist,N,histsize)
 %HISTOGRAMA_N Summary of this function goes here
 %   Detailed explanation goes here
 % Funcion que dado un histograma de etiquetas y un dato de entrada
 % devuelve la etiqueta que le corresponde dependiendo de N
+DATOSPORCLASE = histsize.maxbin - histsize.minbin;
+minvalue = 1;
+maxvalue = N;
 
 xn = floor(DATOENTRADA(1)/(DATOSPORCLASE/N))+1;
 if(xn < minvalue)
