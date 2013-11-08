921dda5450ba7d4deebf520e5e26275076ca9e41
oriong2@gmail.com <oriong2@gmail.com@fac77a9e-9cf5-8a52-1d01-95cccab20d58>
2013-11-08 16:21:28 +0000

    Parzen clasification

--- Practise1/practica1_codigo_inicial/parzen_h.m
+++ Practise1/practica1_codigo_inicial/parzen_h.m
@@ -9,21 +9,30 @@ function LABEL = parzen_h(DATOENTRADA, DATOS, ETIQUETAS, INDICES, hvalue)
 [r, c] = size(INDICES);
 
 NUMTRAININGCLASES = r;
-f = zeros(NUMTRAININGCLASES);
+f = zeros(1,NUMTRAININGCLASES);
+dist = ones(1,NUMTRAININGCLASES)*bitmax;
 
 for i = 1:NUMTRAININGCLASES
     DATOSCLASE = DATOS(ETIQUETAS==i,:);
     NUMINDICESCLASE = length(INDICES(i,:));
 
     for j = 1:NUMINDICESCLASE
-        u = (DATOENTRADA - DATOSCLASE(INDICES(i,j),:))/hvalue;
+        u = norm(DATOENTRADA - DATOSCLASE(INDICES(i,j),:))/hvalue;
         if(abs(u)<=1/2)
             f(i) = f(i)+1;
+            dist(i) = dist(i)+ (u*hvalue);
         end;
     end;
 end;
-[m, n] = sort(f);
-LABEL = n;
+[m, n] = sort(f,'descend');
+if(1 < length(m) && m(1) == m(2))
+    % En caso de empate nos quedamos con la etiqueta de menor distancia    
+    [m, n] = min(dist);    
+    LABEL = n(1);
+else
+    % Devolvemos la etiqueta de la clase con mayor cuenta
+    LABEL = n(1);
+end;
 
 end
 
