dd0f3c283c8aeb9df35ae5326e99fbdea9bc59b8
oriong2@gmail.com <oriong2@gmail.com@fac77a9e-9cf5-8a52-1d01-95cccab20d58>
2013-11-24 18:54:25 +0000

    Added files Load images completed

--- trunk/Practise2/Practise2.m
+++ trunk/Practise2/Practise2.m
@@ -1,12 +1,14 @@
-function [ output_args ] = Practise2( input_args )
+function Practise2
 %PRACTISE2 Summary of this function goes here
 %   Detailed explanation goes here
-X = [2 4 3; 4 5 6; 2 5 6; 3 5 8; 4 3 2; 7 4 1;];
-y = [1;1;2;2;3;3];
+%X = [2 4 3; 4 5 6; 2 5 6; 3 5 8; 4 3 2; 7 4 1;];
+%y = [1;1;2;2;3;3];
 
-A = LDA(X,y);
+[X, LABELS]= LoadImages;
+checkLDAQuality(X, LABELS);
+    
+A = LDA(X,LABELS);
 
-A
 
 end
 
