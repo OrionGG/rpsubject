ce6efceb04ead89185f18e630078a1761520900f
oriong2@gmail.com <oriong2@gmail.com@fac77a9e-9cf5-8a52-1d01-95cccab20d58>
2013-11-24 22:14:27 +0000

    drawing LDA result

--- trunk/Practise2/checkLDAQuality.m
+++ trunk/Practise2/checkLDAQuality.m
@@ -9,37 +9,68 @@ classLabels = unique(LABELS);
 CLASSNUMBER = length(classLabels);
 
 % We select data randomly to do n-fold
-randpermClases = zeros(CLASSNUMBER,length(X)/CLASSNUMBER);
 for i=1:CLASSNUMBER
     DATAOFCLASS = X(LABELS==i,:);
-    randpermClases(i,:) = randperm(length(DATAOFCLASS));
-    DATAOFCLASS = zeros(size(DATAOFCLASS,1),size(DATAOFCLASS,2));
+    NumDataInClass = size(DATAOFCLASS,1);
+    randpermClases{i} = randperm(NumDataInClass);
+    
+    NumDataInTest = round(NumDataInClass/NFOLD);
+    testIndexes{i} = zeros(1, NumDataInTest);
+    trainIndexes{i} = zeros(1, NumDataInClass - NumDataInTest);
 end;
 
-for j = 1:NFOLD   
-    lengthMeanByClass = length(X)/NUM_CLASES;
-    lengthMeanByClassByFold = lengthMeanByClass/NFOLD;
-    
-    testIndexes = zeros(NUM_CLASES, lengthMeanByClassByFold);
-    trainIndexes = zeros(NUM_CLASES, lengthMeanByClass - lengthMeanByClassByFold);
-    
-    %Generación de indices de entrenamiento y test
-    for i=1:NUM_CLASES
+
+
+for j = 1:NFOLD
+    dataTest = [];
+    dataTrain = [];
+    labelsTrain = [];
+    % We select data randomly to do n-fold
+    for i=1:CLASSNUMBER
         DATAOFCLASS = X(LABELS==i,:);
+        NumDataInClass = size(DATAOFCLASS,1);
+        NumDataInTest = round(NumDataInClass/NFOLD);
         
-        lengthByClassByFoldTest = length(DATAOFCLASS)/NFOLD;
-        lengthByClassByFoldTrain = length(DATAOFCLASS) - length(DATAOFCLASS)/NFOLD;
+        endTestIndexes = min(NumDataInClass, j * NumDataInTest);
+        testIndexes{i} = randpermClases{i}((1+((j-1)* NumDataInTest)):endTestIndexes);
+        trainIndexes{i} = randpermClases{i}(1:((j-1)* NumDataInTest));
+        trainIndexes{i} = [trainIndexes{i},randpermClases{i}((j*NumDataInTest)+1:NumDataInClass)];
         
-        %indices de los datos para datos de test
-        testIndexes(i,:) = randpermClases(i, (1+((j-1)*lengthByClassByFoldTest)):j*lengthByClassByFoldTest);
-        %indices de los datos para los datos de entrenamiento
-        trainIndexes(i,1:((j-1)*lengthByClassByFoldTest)) = randpermClases(i,1:((j-1)*lengthByClassByFoldTest));
-        trainIndexes(i,(1+((j-1)*lengthByClassByFoldTest)):lengthByClassByFoldTrain) = randpermClases(i,(j*lengthByClassByFoldTest)+1:length(DATAOFCLASS));
+        %dataTest = [dataTest; DATAOFCLASS(testIndexes{i},:)];
+        dataTest{i} = DATAOFCLASS(testIndexes{i},:);
+        dataTrain = [dataTrain; DATAOFCLASS(trainIndexes{i},:)];
         
-        DATAOFCLASS = zeros(size(DATAOFCLASS,1),size(DATAOFCLASS,2));
+        LABELCLASS = LABELS(LABELS==i);
+        labelsTrain = [labelsTrain; LABELCLASS(trainIndexes{i})];
+        %LABELCLASSTRAIN = ones(1,length(dataTrain))*i;
+        
+        
+        testIndexes{i} = zeros(1, NumDataInTest);
+        trainIndexes{i} = zeros(1, NumDataInClass - NumDataInTest);
     end;
+    
+    A = LDA(dataTrain, labelsTrain);
+    
+    figure
+    COLORES{1} = [255; 0; 0];
+    COLORES{2} = [0; 255; 0];
+    COLORES{3} = [0; 0; 255];
+    COLORES{4} = [0; 0; 0];
+    
+    for i=1:CLASSNUMBER
+        proyection{i} = dataTest{i}*A;
+        
+        color    = COLORES{i};
+        r        = color(1)/255;
+        g        = color(2)/255;
+        b        = color(3)/255;
+        
+        plot3(proyection{i}(:,1), proyection{i}(:,2), proyection{i}(:,3), 'x', 'Color', [r,g,b] );
+        hold on;
+    end;
+    grid on
+    axis square
+    hold off;
 end;
-
-
 end
 
