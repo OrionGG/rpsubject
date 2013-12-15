68cc48fe10467773912bfc2812608ea0bac4052a
oriong2@gmail.com <oriong2@gmail.com@fac77a9e-9cf5-8a52-1d01-95cccab20d58>
2013-12-15 21:46:49 +0000

    Rewritted check of LDA

--- Practise2/code/checkSW.m
+++ Practise2/code/checkSW.m
@@ -1,13 +1,8 @@
-function checkSW( X, LABELS)
+function [RCONDSt] = checkSW( X, LABELS)
 %CHECKSW check if Sw is singular after doing LDA to data
 
-
-A = LDA(X, LABELS);
-
-proyectedX = X*A;
-
 % Determine size of input data
-[m n] = size(proyectedX);
+[m n] = size(X);
 
 % Count class labels
 classLabel = unique(LABELS);
@@ -16,7 +11,7 @@ CLASSNUMBER = length(classLabel);
 % same number of data in each class
 meanX = zeros(CLASSNUMBER,n);
 for i = 1:CLASSNUMBER
-    DATAOFCLASS = proyectedX(LABELS==i,:);
+    DATAOFCLASS = X(LABELS==i,:);
     meanX(i,:) = mean(DATAOFCLASS);
 end;
 meanAll = mean(meanX);
@@ -26,23 +21,25 @@ Sw = zeros(n,n);
 for i = 1:CLASSNUMBER    
     Sb = Sb + (meanX(i,:) - meanAll)'*(meanAll-meanX(i,:));
     
-    DATAOFCLASS = proyectedX(LABELS==i,:);
+    DATAOFCLASS = X(LABELS==i,:);
     d = DATAOFCLASS-repmat(meanX(i,:),size(DATAOFCLASS,1),1);
     sumd = d'*d;
     Sw = Sw + sumd;
 end;
 %sprintf('RANK Sw = %d', rank(Sw))
-%sprintf('RCOND Sw = %d', rcond(Sw))
+sprintf('RCOND Sw = %d', rcond(Sw))
 
 %sprintf('RANK ASwAt = %d', rank(A*Sw*A'))
-sprintf('RCOND ASwAt = %d', rcond(A*Sw*A'))
+%sprintf('RCOND ASwAt = %d', rcond(A*Sw*A'))
 
 k = min(rank(Sw), rank(Sb));
 sprintf('Max k = %d', k)
 St = Sw + Sb;
 %sprintf('RANK St = %d', rank(St))
-%sprintf('RCOND St = %d', rcond(St))
-sprintf('RCOND ASwAt = %d', rcond(A*St*A'))
+RCONDSt = rcond(St);
+sprintf('RCOND St = %d', rcond(St))
+%RCONDAStAt = rcond(A*St*A');
+%sprintf('RCOND AStAt = %d', rcond(A*St*A'))
 
 end
 
